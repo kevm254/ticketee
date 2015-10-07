ae061fefba4bb2975d49d2d7ab92c97ab883a0e2
kevm254 <kevm2@yahoo.com>
2015-10-07 02:21:39 -0500

    Link tickets and users upon ticket creatin

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  belongs_to :author, class_name: "User"
   validates :name, :description, presence: true, length: { minimum: 10 }
   validates :description, length: { maximum: 1000 }
 end
