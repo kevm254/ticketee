25d5154a5e8827c367375e2158e2f4f59223fb9f
kevm254 <kevm2@yahoo.com>
2015-10-17 23:23:15 -0500

    Added permissions for editing tickets

--- spec/features/admin/hidden_links_spec.rb
+++ spec/features/admin/hidden_links_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature "Users can only see the appropriate links" do
   let(:project) { FactoryGirl.create(:project) }
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:user, :admin) }
+  let(:ticket) do
+    FactoryGirl.create(:ticket, project: project, author: user)
+  end
 
   context "anonymous users" do
     scenario "cannot see the New Project link" do
@@ -17,6 +20,11 @@ RSpec.feature "Users can only see the appropriate links" do
     before do
       login_as(user)
       assign_role!(user, :viewer, project)
+    end
+
+    scenario "cannot see the Edit Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link "Edit Ticket"
     end
 
     scenario "cannot see the New Ticket link" do
@@ -46,6 +54,11 @@ RSpec.feature "Users can only see the appropriate links" do
     scenario "can see the New Project link" do
       visit "/"
       expect(page).to have_link "New Project"
+    end
+
+    scenario "can see the Edit Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link "Edit Ticket"
     end
 
     scenario "can see the Delete Project link" do
--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe TicketPolicy do
       let(:user) { nil }
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for viewers of the project" do
@@ -19,6 +20,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for editors of the project" do
@@ -26,13 +28,21 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should_not permit_action :update }
+
+      context "when the editor created the ticket" do
+        before { ticket.author = user }
+        it { should permit_action :update }
+      end
     end
+
 
     context "for managers of the project" do
       before { assign_role!(user, :manager, project) }
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
 
     context "for managers of other projects" do
@@ -42,6 +52,7 @@ RSpec.describe TicketPolicy do
 
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for administrators" do
@@ -49,6 +60,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
   end
 end
