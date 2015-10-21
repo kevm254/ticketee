cd0487f21b2bfa386d54070853998c84148db638
kevm254 <kevm2@yahoo.com>
2015-10-21 12:29:36 -0500

    Admins can now set a default state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
         patch :archive
       end
     end
-    resources :states, only: [:index, :new, :create]
+    resources :states, only: [:index, :new, :create] do
+      member do
+        get :make_default
+      end
+    end
   end
 
   devise_for :users
@@ -22,5 +26,4 @@ Rails.application.routes.draw do
   end
 
   resources :attachments, only: [:show, :new]
-
 end
