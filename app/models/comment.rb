dfd054ae5f95890f7e943a1fc6e7d1bbf75d1c38
kevm254 <kevm2@yahoo.com>
2015-10-23 23:41:08 -0500

    Automatically subscribe users to a ticket when they comment on it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   after_create :set_ticket_state
   after_create :associate_tags_with_ticket
+  after_create :author_watches_ticket
 
   attr_accessor :tag_names
 
@@ -31,6 +32,12 @@ class Comment < ActiveRecord::Base
       tag_names.split.each do |name|
         ticket.tags << Tag.find_or_create_by(name: name)
       end
+    end
+  end
+
+  def author_watches_ticket
+    if author.present? && !ticket.watchers.include?(author)
+      ticket.watchers << author
     end
   end
 end
